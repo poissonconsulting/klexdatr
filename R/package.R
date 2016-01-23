77f1f4e0e38343b1a5fd645ac2cfb9dd59e8180b
Joe Thorley <joe@poissonconsulting.ca>
2016-01-23 14:50:38 -0800

    examples

--- R/package.R
+++ R/package.R
@@ -4,4 +4,20 @@
 #'
 #' @docType package
 #' @name klexdatr
+#' @examples
+#' \dontrun{
+#' library(dplyr) # so tbl data frames print nice
+#' library(klexdatr) # data set for example
+#' library(lexr)
+#'
+#' klex <- input_lex_data("klexdatr")
+#' check_lex_data(klex)
+#' print(klex)
+#' plot(klex, all = TRUE)
+#'
+#' kdetect <- make_detect_data(klex)
+#' check_detect_data(kdetect)
+#' print(kdetect)
+#' plot(kdetect)
+#' }
 NULL
